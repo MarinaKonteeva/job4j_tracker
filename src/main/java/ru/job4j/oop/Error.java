50f2b8f888d2501bb63fb6c539c00ed59438911c
home <chujkova@mail.ru>
2022-01-06 13:48:18 +0500

    .7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error err = new Error(true, 10, "ok");
+        err.printInfo();
+        Error er = new Error(false, 5, "No");
+        er.printInfo();
+    }
+}
