f323023e0923f1b9e5625fdeac4dc91efb65ecef
home <chujkova@mail.ru>
2022-01-09 19:21:20 +0500

    4. Расстояние между точками в трехмерном пространстве. [#122643 #248258]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,44 +1,44 @@
 package ru.job4j.oop;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import static java.lang.Math.sqrt;
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
-public class PointTest extends TestCase {
+public class PointTest {
 
-    public void testDistance() {
+    @Test
+    public void testDistance5() {
         Point first = new Point(0, 0);
         Point second = new Point(5, 0);
-
         double result = first.distance(second);
         double expected = 5;
         assertThat(result, closeTo(expected, 0.001));
     }
 
-    public void testDistance2() {
+    @Test
+    public void testDistanceSqrt2() {
         Point first = new Point(0, 0);
         Point second = new Point(1, 1);
-
         double result = first.distance(second);
         double expected = sqrt(2);
         assertThat(result, closeTo(expected, 0.001));
     }
 
-    public void testDistance3d() {
+    @Test
+    public void testDistance3d5() {
         Point first = new Point(0, 0, 0);
         Point second = new Point(5, 0, 0);
-
         double result = first.distance3d(second);
         double expected = 5;
         assertThat(result, closeTo(expected, 0.001));
     }
 
-    public void testDistance3d2() {
+    @Test
+    public void testDistance3dSqrt3() {
         Point first = new Point(0, 0, 0);
         Point second = new Point(1, 1, 1);
-
         double result = first.distance3d(second);
         double expected = sqrt(3);
         assertThat(result, closeTo(expected, 0.001));
