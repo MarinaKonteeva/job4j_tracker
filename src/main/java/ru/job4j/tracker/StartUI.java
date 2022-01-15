d4861a6c7735e62ec3683ca72aaaf4d12573b618
home <chujkova@mail.ru>
2022-01-15 09:55:43 +0500

    6. Date. Отображение даты. [#310017 #250838]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,13 +7,10 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        item.getCreated();
-
-        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime itemCreated = item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = currentDateTime.format(formatter);
-
-        System.out.println(currentDateTimeFormat);
+        String itemCreatedFormat = itemCreated.format(formatter);
+        System.out.println(itemCreatedFormat);
     }
 
 }
