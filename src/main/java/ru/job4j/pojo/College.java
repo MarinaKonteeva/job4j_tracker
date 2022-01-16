e12355388ff751032d6d79a6e4c09c6b7fac37d1
home <chujkova@mail.ru>
2022-01-16 15:43:04 +0500

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Иванов Иван Иванович");
+        student.setGroup("MX-101");
+        student.setEntry(new Date());
+
+        System.out.println("Студент: " + student.getName()
+                + ". Группа: " + student.getGroup());
+    }
+}
