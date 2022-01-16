a6c40b2b5b00716576e8809a2c2f35c58c92a98f
home <chujkova@mail.ru>
2022-01-16 19:30:04 +0500

    3. Массивы и модели. [#174892 #251642]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -3,30 +3,25 @@ package ru.job4j.pojo;
 import java.sql.SQLOutput;
 
 public class Library {
-    public static void main(String[] args) {
-        Book book1 = new Book("Пост", 400);
-        Book book2 = new Book("Текст", 300);
-        Book book3 = new Book("Метро", 3000);
-        Book book4 = new Book("Clean code", 500);
-        Book[] books = new Book[4];
-        books[0] = book1;
-        books[1] = book2;
-        books[2] = book3;
-        books[3] = book4;
+    private static void printArray(Book[] books) {
         for (int i = 0; i < books.length; i++) {
             Book book = books[i];
             System.out.println(book.getName() + " - " + book.getStr());
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Book[] books = new Book[4];
+        books[0] = new Book("Пост", 400);
+        books[1] = new Book("Текст", 300);
+        books[2] = new Book("Метро", 3000);
+        books[3] = new Book("Clean code", 500);
+        printArray(books);
         Book b = books[0];
         books[0] = books[3];
         books[3] = b;
-
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
-            System.out.println(book.getName() + " - " + book.getStr());
-        }
-        System.out.println();
+        printArray(books);
         for (int i = 0; i < books.length; i++) {
             Book book = books[i];
             if (book.getName().equals("Clean code")) {
