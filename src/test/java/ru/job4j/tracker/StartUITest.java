7dabd90d4e06a95712b8a544424ff86c544a5637
home <chujkova@mail.ru>
2022-02-05 16:19:19 +0500

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #260313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -146,4 +146,16 @@ public class StartUITest {
                 )
         );
     }
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
 }
