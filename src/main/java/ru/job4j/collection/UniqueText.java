b140bd03c08f5b15d5f082bcc671423d01311dc3
home <chujkova@mail.ru>
2022-02-20 10:34:32 +0500

    2. Идентичные тексты [#212655 #266855]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -3,6 +3,7 @@ package ru.job4j.collection;
 import java.util.HashSet;
 
 public class UniqueText {
+
     public static boolean isEquals(String originText, String duplicateText) {
         boolean rsl = true;
         String[] origin = originText.split(" ");
