0e24ddb9efe22b83f36d6c2aee58fb0b86492edf
home <chujkova@mail.ru>
2022-03-07 20:13:10 +0500

    2. Комбинированный компаратор [#10036 #271761]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -11,6 +11,7 @@ import java.util.Comparator;
 import java.util.List;
 
 import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class JobTest {
@@ -18,8 +19,8 @@ public class JobTest {
     public void whenCompatorByNameAndPrority() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 1),
+                new Job("Impl task", 0)
         );
         assertThat(rsl, lessThan(0));
     }
