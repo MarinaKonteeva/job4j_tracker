55bb73b57662187fc68d11d0016837e2bb5d1396
home <chujkova@mail.ru>
2022-03-18 16:36:38 +0500

    2. Отсортировать департаменты [#34352 #276402]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -6,25 +6,9 @@ public class DepDescComp implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        String[] o11 = o1.split("/");
-        String[] o22 = o2.split("/");
-        int len = Math.min(o11.length, o22.length);
-        int rsl = o22[0].compareTo(o11[0]);
-        if (rsl == 0 && len > 1) {
-            rsl = o11[1].compareTo(o22[1]);
-            if (rsl == 0 && len > 2) {
-                rsl = o11[2].compareTo(o22[2]);
-                if (rsl == 0 && len > 2) {
-                    rsl = 0;
-                } else if (rsl == 0 && len == 2) {
-                    rsl = Integer.compare(o11.length, o22.length);
-                }
-            } else if (rsl == 0 && len == 2) {
-                rsl = Integer.compare(o11.length, o22.length);
-            }
-        } else if (rsl == 0 && len == 1) {
-            rsl = Integer.compare(o11.length, o22.length);
-        }
-        return rsl;
+        String[] left = o1.split("/");
+        String[] right = o2.split("/");
+        int rsl = right[0].compareTo(left[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
