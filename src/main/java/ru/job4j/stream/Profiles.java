29984f545987d227f4f6c64a12788d58298037dd
home <chujkova@mail.ru>
2022-03-29 18:01:53 +0500

    2. Список адресов [#110062 #281802]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-
         return profiles.stream()
                 .map(p -> p.getAddress())
                 .collect(Collectors.toList());
